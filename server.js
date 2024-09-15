cde8811aaf2e84b25644403c5314ca06fac6758c
vjnnxx <florianovinicius09@gmail.com>
2024-09-15 12:25:34 -0300

    Requests agora funcionam via json

--- server.js
+++ server.js
@@ -14,12 +14,7 @@ const port = 3000;
 const baseAdress = process.env.BASE;
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-
-// app.use((req, res, next)=>{
-//     res.header('Access-Control-Allow-Origin', '*');
-//     next();
-// });
+app.use(express.json())
 
 app.use(cors());
 
